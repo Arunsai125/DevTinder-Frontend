a361ec7e4ab1cb0720f7ef3fdf70a49571a6dfd6
Arun Sai Narla <arunsaiknr@gmail.com>
2025-12-18 20:46:52 -0500

    feat: update Footer component layout and copyright information

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,14 +3,9 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="footer footer-horizontal footer-center bg-accent text-base-content rounded p-10 fixed bottom-0">
-      <nav className="grid grid-flow-col gap-4">
-        <a className="link link-hover">About us</a>
-        <a className="link link-hover">Contact</a>
-        <a className="link link-hover">Jobs</a>
-        <a className="link link-hover">Press kit</a>
-      </nav>
+      <nav className="grid grid-flow-col gap-2"></nav>
       <nav>
-        <div className="grid grid-flow-col gap-4">
+        <div className="grid grid-flow-col gap-2">
           <a>
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -48,8 +43,8 @@ const Footer = () => {
       </nav>
       <aside>
         <p>
-          Copyright © {new Date().getFullYear()} - All right reserved by ACME
-          Industries Ltd
+          Copyright © {new Date().getFullYear()} - All rights reserved by
+          DevTinder Industries Ltd
         </p>
       </aside>
     </footer>
