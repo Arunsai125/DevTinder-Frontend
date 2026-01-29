532c7e3ab541f23f445cd7f3c0b92e410897e003
Arun Sai Narla <arunsaiknr@gmail.com>
2026-01-28 20:45:27 -0500

    feat: integrate user dispatch in Login component for state management

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,17 +1,21 @@
 import { useState } from "react";
 import axios from "axios";
+import { useDispatch } from "react-redux";
+import { addUser } from "./utils/userSlice";
 
 const Login = () => {
   const [emailId, setEmailId] = useState("");
   const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
   const handleLogin = async () => {
     try {
-      const data = await axios.post(
+      const res = await axios.post(
         "http://localhost:8080/login",
         { emailId, password },
         { withCredentials: true }
       );
-      console.log(data);
+      console.log(res);
+      dispatch(addUser(res.data));
     } catch (err) {
       console.error(err.message);
     }
