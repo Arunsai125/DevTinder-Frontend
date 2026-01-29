01b77620ea2d8b88da49687498761c63384413b3
Arun Sai Narla <arunsaiknr@gmail.com>
2026-01-28 19:52:47 -0500

    feat: integrate Redux store and user slice for state management

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,22 @@ import Body from "./Body";
 import Login from "./Login";
 import Profile from "./Profile";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
 
 function App() {
   return (
     <div>
-      <BrowserRouter basename="/">
-        <Routes>
-          <Route path="/" element={<Body />}>
-            <Route path="/login" element={<Login />} />
-            <Route path="/profile" element={<Profile />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <Provider store={appStore}>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={<Body />}>
+              <Route path="/login" element={<Login />} />
+              <Route path="/profile" element={<Profile />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </div>
   );
 }
