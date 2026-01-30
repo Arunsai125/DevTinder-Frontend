f7f6f99bdd9912bc1138927c80e266ddd106e666
Arun Sai Narla <arunsaiknr@gmail.com>
2026-01-30 16:04:20 -0500

    feat: add UserCard component and update Feed to display user information

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -3,16 +3,20 @@ import { useDispatch, useSelector } from "react-redux";
 import { BASE_URL } from "../utils/constants";
 import { useEffect } from "react";
 import { addFeed } from "../utils/feedSlice";
+import UserCard from "./UserCard";
 
 const Feed = () => {
   const dispatch = useDispatch();
   const feed = useSelector((store) => store.feed);
+
   const getFeed = async () => {
     try {
       if (feed && feed.length > 0) return;
+
       const res = await axios.get(BASE_URL + "/user/feed", {
         withCredentials: true,
       });
+
       const feedData = res.data?.data?.userFeedData ?? [];
       dispatch(addFeed(feedData));
     } catch (err) {
@@ -27,21 +31,11 @@ const Feed = () => {
   if (!feed || feed.length === 0) {
     return <p className="text-center mt-10">No feed available</p>;
   }
+
   return (
-    <div className="card bg-red-500 w-96 shadow-sm mx-auto my-10">
-      <figure>
-        <img src={feed[0].photoUrl} alt="User DP" />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">Card Title</h2>
-        <p>
-          A card component has a figure, a body part, and inside body there are
-          title and actions parts
-        </p>
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary bg-blue-500 p-2">Buy Now</button>
-        </div>
-      </div>
+    <div>
+      {/* For now we show the first user */}
+      <UserCard user={feed[0]} />
     </div>
   );
 };
---
+++ src/components/UserCard.jsx
@@ -0,0 +1,28 @@
+const UserCard = ({ user }) => {
+  if (!user) return null;
+
+  return (
+    <div className="card bg-slate-800 w-96 shadow-sm mx-auto my-10">
+      <figure>
+        <img src={user.photoUrl} alt={`${user.firstName} DP`} />
+      </figure>
+
+      <div className="card-body">
+        <h2 className="card-title">
+          {user.firstName} {user.lastName}
+        </h2>
+
+        <p>{user.about}</p>
+
+        <div className="card-actions justify-center p-2">
+          <button className="btn btn-secondary bg-blue-500 p-2">Ignore</button>
+          <button className="btn btn-primary bg-pink-600 p-2">
+            Interested
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserCard;
