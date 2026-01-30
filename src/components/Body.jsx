d3d50bb67b0f1b1e241b81ae11c789e316c6929e
Arun Sai Narla <arunsaiknr@gmail.com>
2026-01-29 19:48:15 -0500

    feat: implement profile fetching in Body component and handle navigation on unauthorized access

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,27 @@
 import NavBar from "./NavBar";
 import Footer from "./Footer";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useEffect } from "react";
 
 const Body = () => {
+  const fetch = async () => {
+    try {
+      const res = await axios.get(BASE_URL + "/profile/view");
+      useDispatch(addUser(res));
+    } catch (err) {
+      if (err.status === 401) {
+        useNavigate("/login");
+      }
+      console.error(err);
+    }
+  };
+  useEffect(() => {
+    fetch();
+  }, []);
   return (
     <div>
       <NavBar />
