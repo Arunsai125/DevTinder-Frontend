ff423ff86b53ce5b1d8409b9b2b5b02d70870451
Arun Sai Narla <arunsaiknr@gmail.com>
2026-02-02 21:31:37 -0500

    feat: set default age value in EditProfile component and update user photo URL handling in NavBar component

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -9,7 +9,7 @@ import UserCard from "./UserCard";
 const EditProfile = ({ user }) => {
   const [firstName, setFirstName] = useState(user?.firstName || "");
   const [lastName, setLastName] = useState(user?.lastName || "");
-  const [age, setAge] = useState(user?.age || "");
+  const [age, setAge] = useState(user?.age || 18);
   const [about, setAbout] = useState(user?.about || "");
   const [gender, setGender] = useState(user?.gender || "");
   const [photoUrl, setPhotoUrl] = useState(user?.photoUrl || "");
